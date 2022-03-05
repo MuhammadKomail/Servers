3e603751af126f65e09d99a809ddff82118d34bd
MuhammadKomail <92737744+MuhammadKomail@users.noreply.github.com>
2022-03-05 14:24:22 +0500

    Final Almost After Adding 20 images

--- routing/cards.js
+++ routing/cards.js
@@ -13,12 +13,26 @@ router.route('/add').post((request, response) => {
     const description = request.body.description;
     const orignalPrice = request.body.orignalPrice;
     const discountPrice = request.body.discountPrice;
-    // const dorignalPrice = request.body.dorignalPrice;
-    // const ddiscountPrice = request.body.ddiscountPrice;
     const imageUrl1 = request.body.imageUrl1;
     const imageUrl2 = request.body.imageUrl2;
     const imageUrl3 = request.body.imageUrl3;
     const imageUrl4 = request.body.imageUrl4;
+    const imageUrl5 = request.body.imageUrl5;
+    const imageUrl6 = request.body.imageUrl6;
+    const imageUrl7 = request.body.imageUrl7;
+    const imageUrl8 = request.body.imageUrl8;
+    const imageUrl9 = request.body.imageUrl9;
+    const imageUrl10 = request.body.imageUrl10;
+    const imageUrl11 = request.body.imageUrl11;
+    const imageUrl12 = request.body.imageUrl12;
+    const imageUrl13 = request.body.imageUrl13;
+    const imageUrl14 = request.body.imageUrl14;
+    const imageUrl15 = request.body.imageUrl15;
+    const imageUrl16 = request.body.imageUrl16;
+    const imageUrl17 = request.body.imageUrl17;
+    const imageUrl18 = request.body.imageUrl18;
+    const imageUrl19 = request.body.imageUrl19;
+    const imageUrl20 = request.body.imageUrl20;
     const fabric = request.body.fabric;
     const availability = request.body.availability;
     const quantity = request.body.quantity;
@@ -35,12 +49,26 @@ router.route('/add').post((request, response) => {
         description,
         orignalPrice,
         discountPrice,
-        // dorignalPrice,
-        // ddiscountPrice,
         imageUrl1,
         imageUrl2,
         imageUrl3,
         imageUrl4,
+        imageUrl5,
+        imageUrl6,
+        imageUrl7,
+        imageUrl8,
+        imageUrl9,
+        imageUrl10,
+        imageUrl11,
+        imageUrl12,
+        imageUrl13,
+        imageUrl14,
+        imageUrl15,
+        imageUrl16,
+        imageUrl17,
+        imageUrl18,
+        imageUrl19,
+        imageUrl20,
         fabric,
         availability,
         quantity,
@@ -79,12 +107,26 @@ router.route('/update/:id').post((request, response) => {
             card.description = request.body.description;
             card.orignalPrice = request.body.orignalPrice;
             card.discountPrice = request.body.discountPrice;
-            // card.dorignalPrice = request.body.dorignalPrice;
-            // card.ddiscountPrice = request.body.ddiscountPrice;
             card.imageUrl1 = request.body.imageUrl1;
             card.imageUrl2 = request.body.imageUrl2;
             card.imageUrl3 = request.body.imageUrl3;
             card.imageUrl4 = request.body.imageUrl4;
+            card.imageUrl5 = request.body.imageUrl5;
+            card.imageUrl6 = request.body.imageUrl6;
+            card.imageUrl7 = request.body.imageUrl7;
+            card.imageUrl8 = request.body.imageUrl8;
+            card.imageUrl9 = request.body.imageUrl9;
+            card.imageUrl10 = request.body.imageUrl10;
+            card.imageUrl11 = request.body.imageUrl11;
+            card.imageUrl12 = request.body.imageUrl12;
+            card.imageUrl13 = request.body.imageUrl13;
+            card.imageUrl14 = request.body.imageUrl14;
+            card.imageUrl15 = request.body.imageUrl15;
+            card.imageUrl16 = request.body.imageUrl16;
+            card.imageUrl17 = request.body.imageUrl17;
+            card.imageUrl18 = request.body.imageUrl18;
+            card.imageUrl19 = request.body.imageUrl19;
+            card.imageUrl20 = request.body.imageUrl20;
             card.fabric = request.body.fabric;
             card.availability = request.body.availability;
             card.quantity = request.body.quantity;
